60c9e7243f79432d573c515690d8fb5ee43868fc
tomeytran <tmtri310251@gmail.com>
2024-09-16 09:30:09 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // giong bai nobita set position
+
+let noButton = document.getElementById("noBtn");
+function noBtn() {
+	let x = Math.round(Math.random() * window.innerWidth);
+	let y = Math.round(Math.random() * window.innerHeight);
+	noButton.style.left = x + "px";
+	noButton.style.top = y + "px";
+}
